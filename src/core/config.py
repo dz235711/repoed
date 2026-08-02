0f1322067e86c4c9b3c4dc35fc46821f0d4fc48c
Dingyan Zhang <dz1223@ic.ac.uk>
2026-08-02 21:59:52 +0100

    feat: parser settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,23 +1,28 @@
 from tomllib import load as load_toml
 from pydantic import BaseModel
-from enums import VCS
+from enums import VCS, Language
+
+class Git(BaseModel):
+    respect_gitignore: bool
 
 class Repository(BaseModel):
     root: str
     vcs: VCS
+    git: Git
 
 class Ignore(BaseModel):
     extra: list[str]
 
+class Indexing(BaseModel):
+    languages: list[Language]
+
 class Config(BaseModel):
     repository: Repository
     ignore: Ignore
+    indexing: Indexing
 
     @classmethod
     def load(cls, path: str) -> "Config":
         with open(path, "rb") as f:
             data = load_toml(f)
         return cls(**data)
-
-
-print(Config.load("../../config.toml"))
--- src/core/enums.py
+++ src/core/enums.py
@@ -7,3 +7,15 @@ class VCS(str, Enum):
     BAZAAR = "bazaar"
     FOSSIL = "fossil"
     CVS = "cvs"
+
+class Language(str, Enum):
+    PYTHON = "python"
+    JAVASCRIPT = "javascript"
+    JAVA = "java"
+    CSHARP = "csharp"
+    RUBY = "ruby"
+    PHP = "php"
+    GO = "go"
+    RUST = "rust"
+    SWIFT = "swift"
+    KOTLIN = "kotlin"
